a84b6dfacf9527343fbf197aa5fd2aa39370b7af
coolkosta <kostia.borzakov@gmail.com>
2024-09-27 00:15:58 +0300

    Определили структуру проекта

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HHTest"
+include(":app")
+include(":feature:favorite")
+include(":features:search")
+include(":features:favorite")
+include(":features:feedback")
+include(":features:message")
+include(":features:profile")
+include(":core")
